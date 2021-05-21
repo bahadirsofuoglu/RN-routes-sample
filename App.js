4e453b45bfd15470023adb8164c10971c5a8dd40
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-05-21 20:44:06 +0300

    BookScreen created. Route added to project

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import BookScreen from './src/screens/BookScreen'
+const navigator = createStackNavigator(
+  {
+    Books: BookScreen
   },
-});
+  {
+    initialRouteName: 'Books',
+    defaultNavigationOptions: {
+      title: 'Books'
+    }
+  }
+)
+
+export default createAppContainer(navigator)
---
+++ src/screens/BookScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+
+const BookScreen = () => {
+  return (
+    <View>
+      <Text>BookScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default BookScreen
