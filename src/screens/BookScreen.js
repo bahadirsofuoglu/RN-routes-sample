bed0dc791a9516d73c0c0d868fffb10227e4f31b
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-05-22 19:05:16 +0300

    navigation added to bookScreen

--- src/screens/BookScreen.js
+++ src/screens/BookScreen.js
@@ -1,14 +1,29 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
 import BookList from '../components/BookList'
-const BookScreen = () => {
+const BookScreen = props => {
   return (
     <View>
-      <BookList />
+      <TouchableOpacity
+        onPress={() => {
+          props.navigation.navigate('BookDetail')
+        }}
+        style={styles.buttonStyles}
+      >
+        <Text>Go to BookDetailScreen</Text>
+      </TouchableOpacity>
+      <BookList navigation={props.navigation} />
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  buttonStyles: {
+    backgroundColor: 'white',
+    borderRadius: 4,
+    padding: 5,
+    marginHorizontal: 7
+  }
+})
 
 export default BookScreen
