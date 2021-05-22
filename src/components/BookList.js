86a8fe7f84fc3cbc0a86913cf022218e51c1b4e0
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-05-22 05:15:43 +0300

    bookList-book.json completed with flatList

---
+++ src/components/BookList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { StyleSheet, View, Text, FlatList } from 'react-native'
+import books from '../../data/books.json'
+const BookList = props => {
+  return (
+    <View>
+      <FlatList
+        data={books}
+        renderItem={({ item }) => <Text>{item.title}</Text>}
+        keyExtractor={book => book.title}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default BookList
--- src/screens/BookScreen.js
+++ src/screens/BookScreen.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { StyleSheet, View, Text } from 'react-native'
-
+import BookList from '../components/BookList'
 const BookScreen = () => {
   return (
     <View>
-      <Text>BookScreen</Text>
+      <BookList />
     </View>
   )
 }
